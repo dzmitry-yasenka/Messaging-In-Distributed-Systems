aae0aa15c7cd54a54cd6b952040f6f30e4c2ba2c
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2023-03-30 22:32:55 +0100

    At least once delivery and prefetch count experiments

--- MessagingInDistributedSystems.Shared/Subscribers/MessageSubscriber.cs
+++ MessagingInDistributedSystems.Shared/Subscribers/MessageSubscriber.cs
@@ -18,20 +18,23 @@ internal sealed class MessageSubscriber : IMessageSubscriber
         Func<TMessage, BasicDeliverEventArgs, Task> handler) where TMessage : class, IMessage
     {
         _channel.ExchangeDeclare(exchange, "topic", false, false, null);
-        
         _channel.QueueDeclare(queue, false, false, false);
         _channel.QueueBind(queue, exchange, routingKey);
         
+        _channel.BasicQos(0, 4, false);
+        
         var consumer = new EventingBasicConsumer(_channel);
-        consumer.Received += async (_, basicDeliverEventArgs
-        ) =>
+        consumer.Received += async (_, basicDeliverEventArgs) =>
         {
             var message = JsonSerializer.Deserialize<TMessage>(Encoding.UTF8.GetString(basicDeliverEventArgs.Body.ToArray()));
-            if (message != null) 
+            if (message != null)
+            {
                 await handler(message, basicDeliverEventArgs);
+                _channel.BasicAck(basicDeliverEventArgs.DeliveryTag, false);
+            }
         };
         
-        _channel.BasicConsume(queue, true, consumer);
+        _channel.BasicConsume(queue, false, consumer);
 
         return this;
     }
